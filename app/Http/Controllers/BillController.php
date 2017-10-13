e7a63bbf80cb94cc2913fc183048998fb8c9f910
Tracy Ericson <tracylericson@gmail.com>
2017-10-12 22:54:17 -0400

    routes and controller

---
+++ app/Http/Controllers/BillController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BillController extends Controller
+{
+    public function index ()
+
+    {return 'Show form bill splitter';
+    }
+
+
+
+    public function calculate()
+    {return 'calculates bills';
+# redirect
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+    //return view('welcome');
+//});
+
+Route:: get ('/', 'BillController@index');
+Route:: get ('/calculate', 'BillController@calculate');
